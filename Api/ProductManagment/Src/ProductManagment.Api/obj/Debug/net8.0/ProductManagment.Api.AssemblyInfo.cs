d263754857bd5574677cc33ad03a5893d293561e
Subin Suresh <subink.s238@gmail.com>
2025-01-26 20:34:09 +0000

    fixed dotnet docker path

--- Api/ProductManagment/Src/ProductManagment.Api/obj/Debug/net8.0/ProductManagment.Api.AssemblyInfo.cs
+++ Api/ProductManagment/Src/ProductManagment.Api/obj/Debug/net8.0/ProductManagment.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagment.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad96f1e3296f400d5a3b18472024e5ffb7fa2907")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b9284d72b0ad1b35cd45849054473d455b58a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagment.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagment.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
